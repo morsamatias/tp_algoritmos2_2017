ed56d20f2968340fd4bc3c177944487ca65acc2e
Leonel Gutierrez <leoguty91@github.com>
2017-08-27 16:00:51 -0300

    Se agregan los annotations de target y retention.

--- XForm/XAnnotation/Control.java
+++ XForm/XAnnotation/Control.java
@@ -1,7 +1,14 @@
 package XAnnotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import XObject.XObject;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Control {
 	String label();
 	Class<? extends XObject> type();
