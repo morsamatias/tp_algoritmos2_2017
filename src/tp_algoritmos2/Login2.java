8d160d409d363f2cecf2197b5e75a5bbec3aa015
leoguty91 <leoguty91@gmail.com>
2017-10-29 21:21:32 -0300

    Se corrige el merge
    Se corrige un mer mal hecho

--- src/tp_algoritmos2/Login2.java
+++ src/tp_algoritmos2/Login2.java
@@ -9,26 +9,43 @@ import XObject.*;
 @Form(name="Login2", title="Pantalla de inicio")
 public class Login2 extends XForm{
 	
-	@Control(label="Usuario", type=TextField.class)
-	private String user;
+	@Control(label="Nombre", type=TextField.class)
+	private String nombre;
 	
-	@Action(label="Test3", method="test3")
-	@Control(label="Hombre?", type=CheckBox.class)
-	private Boolean hombre;
+	@Control(label="Apellido", type=TextField.class)
+	private String apellido;
 	
-	@Control(label="Color", type=RadioBox.class)
-	private String[] color = {"Rojo", "Amarillo"};
+	@Control(label="Nro.de afiliado", type=TextField.class)
+	private String nroAfiliado;
+	
+	@Control(label="Tipo de documento", type=ComboBox.class)
+	private String[] tipoDocumento = {"Dni", "Otros"};
+	
+	@Control(label="Nro.de documento", type=TextField.class)
+	private String nroDoc;
 
-	@Control(label="Dificultad", type=ComboBox.class)
-	private String[] dificultad = {"Facil", "Medio", "Dificil"};
+	@Control(label="sexo", type=RadioBox.class)
+	private String[] sex = {"Masculino", "Femenino"};
+	
+	@Control(label="Esposa", type=CheckBox.class)
+	private String esposa;
+
+	@Control(label="1 hijo", type=CheckBox.class)
+	private String hijo1 ;
 	
 	@Action(label="Test3", method="test3")
+	@Control(label="2 o mas hijos", type=CheckBox.class)
+	private String[] hijo2 ;
+
 	@Control(label="Tabla", type=Table.class)
-	private String[][] tabla = {{"Fila 11", "Fila 12", "Fila 13"}, {"Fila 21", "Fila 22", "Fila 23"}, {"Fila 31", "Fila 32", "Fila 33"}};
+	private String[][] tabla = {{"Especialista","Fecha","Consideraciones"}};
 	
 	public void test3()
 	{
 		System.out.println("Metodo de prueba 3");
+		//@Control(label="Tabla2", type=Table.class) 
+		//public String[][] tabla2 = {{"Especialista","Fecha","Consideraciones"},{"Especialista","Fecha","Consideraciones"}};
+		//No anda esta annotation
 	}
 
 }
